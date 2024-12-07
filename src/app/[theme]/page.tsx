92720f5e6715f541772d50c6f87a4ff9128d5235
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-12-08 00:52:01 +0100

    routing/useParams finally works as intended

--- src/app/[theme]/page.tsx
+++ src/app/[theme]/page.tsx
@@ -3,7 +3,6 @@ import { useParams } from "next/navigation";
 
 export default function QuizzPage() {
     const { theme } = useParams<{ theme: string }>();
-    console.log(theme);
     return (
         <h1> {theme}&apos;s quizz </h1>
     )
