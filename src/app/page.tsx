07c31feab3638c7f3b324dc4109dff0b77f732ae
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-12-16 19:07:20 +0100

    created a hook useFetchQuestions for the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,18 @@
 'use client'
-import { useEffect, useState } from 'react'
-import styles from './page.module.scss'
-import { fetchQuestions } from '@/api/callAPI'
-import { useQuizzContext } from '@/contexts/QuizzContext'
+import React from 'react'
+import { useFetchQuestions } from '@/hooks/useFetchQuestions'
 import ThemeDiv from '@/components/themeDiv/ThemeDiv'
-import extractUniqueThemes from '@/utils/extractUniqueThemes'
+import styles from './page.module.scss'
 
 export default function Home() {
-  const { data, setData } = useQuizzContext()
-  const [themes, setThemes] = useState<string[]>([])
-  const [error, setError] = useState<string | null>(null)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Fetch all words from the API
-        setData(await fetchQuestions())
-        // Extract unique themes from the data
-        setThemes(extractUniqueThemes(data))
-      } catch (err) {
-        setError(
-          err instanceof Error ? err.message : 'An unknown error occurred'
-        )
-        console.error(err)
-      }
-    }
-
-    fetchData()
-  }, [data, error, setData])
+  const { themes, error } = useFetchQuestions()
 
   return (
     <main>
-      <h1>learn Korean!</h1>
+      <h1>Learn Korean!</h1>
       <div className={styles.main_content}>
         <h2>Choose a theme</h2>
-        <ThemeDiv themes={themes} />
+        {error ? <p>{error}</p> : <ThemeDiv themes={themes} />}
       </div>
     </main>
   )
---
+++ src/hooks/useFetchQuestions.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import { fetchQuestions } from '@/api/callAPI'
+import extractUniqueThemes from '@/utils/extractUniqueThemes'
+import { useQuizzContext } from '@/contexts/QuizzContext'
+
+export const useFetchQuestions = () => {
+  const { setData } = useQuizzContext()
+  const [themes, setThemes] = useState<string[]>([])
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const questions = await fetchQuestions()
+        setData(questions)
+        setThemes(extractUniqueThemes(questions))
+      } catch (err) {
+        setError(
+          err instanceof Error ? err.message : 'An unknown error occurred'
+        )
+        console.error(err)
+      }
+    }
+
+    fetchData()
+  }, [setData])
+
+  return { themes, error }
+}
